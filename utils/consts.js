48f909ddc0c09c34cf7fca9c27a1702556b12a4d
Dani BL <danibachsl@protonmail.com>
2022-07-07 10:40:29 +0200

    deploy

--- utils/consts.js
+++ utils/consts.js
@@ -1,3 +1,3 @@
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/Proyecto-crypto-dashboard";
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost/Proyecto-crypto-dashboard";
 
 module.exports = MONGO_URI;
